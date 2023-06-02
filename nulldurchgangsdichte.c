2d70e21a07f2a875fb78bb851505557f58395cc9
KleonDingeldein <121400442+KleonDingeldein@users.noreply.github.com>
2023-06-02 09:34:02 +0200

    Add files via upload

---
+++ nulldurchgangsdichte.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int ndg_dichte(short *feld_ptr, unsigned int anzahl_atw, float *dichte_ori, float *dichte_diff) {
+
+    unsigned int i=0;
+    signed int schwelle=100; // EINSTELLEN!
+
+    // Original-Dichte:
+
+    *dichte_ori=0;
+
+    while(i<anzahl_atw) {
+
+        // größer als +s --> nächsten ermitteln der kleiner ist als -s
+        if(*(feld_ptr+i)>schwelle) {
+            for(i=i; i<anzahl_atw; i++) {
+                if(*(feld_ptr+i)<(-schwelle)) {
+                    (*dichte_ori)++;
+                    break;
+                }
+            }
+        }
+        // kleiner als -s --> Nächsten ermitteln der überhalb von +s ist
+        else if(*(feld_ptr+i)<(-schwelle)) {
+            for(i=i; i<anzahl_atw; i++) {
+                if(*(feld_ptr+i)>schwelle) {
+                    (*dichte_ori)++;
+                    break;
+                }
+            }
+        }
+        // keines von beiden: zähle weiter:
+        else i++;   
+    }
+    *dichte_ori=*dichte_ori/anzahl_atw;
+
+
+    // Differenzierte Dichte:
+
+    i=1;
+    *dichte_diff=0;
+    while(i<anzahl_atw) {
+
+        // größer als +s --> nächsten ermitteln der kleiner ist als -s
+        if((*(feld_ptr+i)-*(feld_ptr+(i-1)))>schwelle) {
+            for(i=i; i<anzahl_atw; i++) {
+                if((*(feld_ptr+i)-*(feld_ptr+(i-1)))<(-schwelle)) {
+                    (*dichte_diff)++;
+                    break;
+                }
+            }
+        }
+
+        // kleiner als -s --> Nächsten ermitteln der überhalb von +s ist
+        else if((*(feld_ptr+i)-*(feld_ptr+(i-1)))<(-schwelle)) {
+            for(i=i; i<anzahl_atw; i++) {
+                if((*(feld_ptr+i)-*(feld_ptr+(i-1)))>schwelle) {
+                    (*dichte_diff)++;
+                    break;
+                }
+            }
+        }
+        // keines von beiden: zähle weiter:
+        else i++;   
+    }
+
+    *dichte_diff=*dichte_diff/anzahl_atw;
+
+    if(*dichte_diff==0 || *dichte_ori == 0) return 1; // Signal zu schwach
+    return 0; // kein Fehler aufgetreten
+}
+
+
+
+
+void main(void) {
+
+    // Beispielsignal
+    short sample[20] = {10,20,130,200,-5,99,-110,13,-170,-400, 113,-2,150,5,-2,-130,200,170,-2,20};
+    // short sample[20] = {10,20,99,20,-5,99,-10,13,-70,-40, 13,-2,10,5,-2,-30,20,70,-2,20}; // schwaches Signal
+    for (int i=0;i<20;i++) {
+        printf("%d ", sample[i]);
+    }
+    printf("\n");
+
+    // Pointer auf Beginn des relevanten Teiles
+    short *beginn;
+    beginn=&sample[0]; //der Pointer "beginn" zeigt auf Adresse des ersten Objektes
+
+    // Breite des relevanten Teiles
+    int anzahl=20;
+
+    // Originaldichte:
+    float dichte_original=0;
+
+    // Differenzierte Dichte:
+    float dichte_differenziert=0;
+
+
+    // Funktionsaufruf Signaldetektion
+    int fehlermeldung=ndg_dichte(beginn, anzahl, &dichte_original, &dichte_differenziert);
+
+
+    printf("Original-Dichte: %f\n", dichte_original);
+    printf("Differenzierte-Dichte: %f\n", dichte_differenziert);
+    printf("Fehlerstatus: %d\n", fehlermeldung);
+}
---
+++ signal_detect.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int signal_detekt(short **sample_anfang, int *sample_anzahl) {
+
+   unsigned int anzahl_zeitintervalle=4; // ABÄNDERN!!
+   unsigned int laenge_zeitintervalle=*sample_anzahl/anzahl_zeitintervalle;
+
+   unsigned int energieverlauf[4]; // ABÄNDERN
+   unsigned int energie=0;
+   unsigned int energie_index=0;
+
+   unsigned int energie_max=0;
+
+   float einstellung_schwelle=0.3; // ABÄNDERN
+
+   unsigned int schwelle;
+
+   unsigned int index_Beginn_Signal;
+
+   // Schritt 1: Energieverlauf bestimmen
+   for(int i=0; i<*sample_anzahl; i++) {
+    energie = energie + (*(*sample_anfang+i)) * (*(*sample_anfang+i));
+
+    if((i+1)%laenge_zeitintervalle==0) {
+        energieverlauf[energie_index] = energie;
+        energie_index++;
+        energie = 0;
+    }
+   }
+
+   // Schritt 2: Schwellwert bestimmen
+   for (int i=0; i<anzahl_zeitintervalle; i++) {
+    if(energieverlauf[i]>energie_max) {
+        energie_max=energieverlauf[i];
+    }
+   }
+   schwelle=energie_max*einstellung_schwelle;
+
+   // Schritt 3: Startwert bestimmen
+   for(int i=0; i<anzahl_zeitintervalle; i++) {
+    if(energieverlauf[i]>schwelle) {
+        **sample_anfang=*(*sample_anfang+(i*laenge_zeitintervalle));
+        index_Beginn_Signal=i;
+        break;
+    }
+    if(i==anzahl_zeitintervalle-1) {
+        return 1; // kein Startwert bestimmbar: Signal kann nicht verarbeitet werden
+    }
+   }
+
+   // Schritt 4: Ende des Signals bestimmen
+   for(int i=index_Beginn_Signal;i<anzahl_zeitintervalle;i++) {
+    if(energieverlauf[i]<schwelle||i==anzahl_zeitintervalle) {
+        *sample_anzahl=(i-index_Beginn_Signal)*laenge_zeitintervalle;
+        return(0);
+        break;
+    }
+   }
+
+}
+
+
+
+
+void main(void) {
+
+    // Beispielsignal
+    short sample[20] = {1,2,3,2,3,9,7,13,17,4,5,-2,-5,3,4,3,2,1,-2,2};
+    for (int i=0;i<20;i++) {
+        printf("%d ", sample[i]);
+    }
+    printf("\n");
+
+    // Pointer auf Beginn des relevanten Teiles
+    short *beginn;
+    beginn=&sample[0]; //der Pointer "beginn" zeigt auf Adresse des ersten Objektes
+
+    // Breite des relevanten Teiles
+    int anzahl=20;
+
+    // Funktionsaufruf Signaldetektion
+    int fehlermeldung=signal_detekt(&beginn, &anzahl);
+
+    printf("Startwert: %d\n", *beginn);
+    printf("Anzahl der relevaten Abtastwerte nach Beginn: %d\n", anzahl);
+}

722eb9e10d874aeb535b72d115ca6eb206b20aa3
Justus <justus.krenkek@googlemail.com>
2023-06-04 19:12:07 +0200

    kleine anpassung im Code

--- nulldurchgangsdichtehistogramm.c
+++ nulldurchgangsdichtehistogramm.c
@@ -11,12 +11,12 @@ int ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, fl
     unsigned int pos_1, pos_2;  // Start- und Entposition eines dedektierten NDG
     short d1=10, d2=40, d3=100; // Grenzen zwischen den Kanälen; ggf. einstellen
     short inc_k1=1, inc_k2=1, inc_k3=1, inc_k4=1;   // Inkremente der Histogrammkanäle; ggf. einstellen
-    signed int schwelle=100; // EINSTELLEN!
+    signed int schwelle=100; // Schwellwert; ggf. EINSTELLEN!
     int returnwert = 0;
 
     // Originalsignal:
     while(i<anzahl_atw) {
-
+ 
         // größer-gleich als +s
         if(*(feld_ptr+i)>=schwelle) {
         
@@ -26,7 +26,7 @@ int ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, fl
             while(*(feld_ptr+i)>=schwelle) {
                 i++;
             }
-            
+
             pos_2 = i;
 
             zuordnung(pos_1, pos_2, &hist_ori, d1, d2, d3, inc_k1, inc_k2, inc_k3, inc_k4);
@@ -109,16 +109,16 @@ int ndg_histogramm(short *feld_ptr, unsigned int anzahl_atw, float *hist_ori, fl
 // Hilfsfunktion - Zuordnung der Abstände in die Kanäle des Histogramms
 void zuordnung (int pos_1, int pos_2, float *hist, short d1, short d2, short d3, int inc_k1, int inc_k2, int inc_k3, int inc_k4) {
 
-    int abstand = 0;
-    abstand = pos_2 - pos_1;
+    int laenge = 0;
+    laenge = pos_2 - pos_1;
 
-    if (abstand <= d1) {
+    if (laenge <= d1) {
         *(hist)+= inc_k1;
     }
-    else if (abstand <=d2) {
+    else if (laenge <=d2) {
         *(hist+1)+=inc_k2;
     }
-    else if (abstand <=d3) {
+    else if (laenge <=d3) {
         *(hist+2)+=inc_k3;
     }
     else {
